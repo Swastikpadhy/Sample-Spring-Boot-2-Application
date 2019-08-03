8459404c6d1505716e990ace90e6ebbd9171c134
swastikpadhy <swastikpadhy@gmail.com>
2019-08-03 22:24:06 +0530

    Initial Project Structure Commit

---
+++ springboot2_dynamic_logging_level/src/main/java/com/Springboot2DynamicLoggingLevelApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2DynamicLoggingLevelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2DynamicLoggingLevelApplication.class, args);
+	}
+
+}
